717c5fcf0d1468e39204ddc8b5405f5d61f1e0d2
miksoyer <miksoyer7@gmail.com>
2025-07-06 01:14:58 +0800

    unh

--- configuration/darwin/gui/controlcenter.nix
+++ configuration/darwin/gui/controlcenter.nix
@@ -5,13 +5,13 @@
     menuExtraClock.ShowDate = 1;            # Show the full date. 0 = When space allows 1 = Always 2 = Never
 
     controlcenter = {
-      AirDrop = false;                    # Show AirDrop in menu bar.
       BatteryShowPercentage = true;       # Show a battery percentage in menu bar
+      AirDrop = false;                    # Show AirDrop in menu bar.
+      Sound = true;                      # Show a sound control in menu bar
       Bluetooth = true;                   # Show a bluetooth control in menu bar. 
       Display = true;                     # Show a Screen Brightness control in menu bar.
-      FocusModes = true;                  # Show a Focus control in menu bar.
+      FocusModes = false;                  # Show a Focus control in menu bar.
       NowPlaying = false;                  # Show a Now Playing control in menu bar.
-      Sound = true;                      # Show a sound control in menu bar
     };
 
     dock = {
