dba681857aeeb5cbd120affadacde5799d248c31
miksoyer <miksoyer7@gmail.com>
2025-07-06 01:32:14 +0800

    edit the initializer script

--- linkstart.sh
+++ linkstart.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
-# install the command line tools(must)
+# 1. open vpn
+
+# 2. install the command line tools(must)
 xcode-select --install
 
-# read this file to disable th SIP for yabai
-# https://github.com/koekeishiya/yabai/wiki/Disabling-System-Integrity-Protection
+# 3. clone the nix dotfile repository
+git clone https://github.com/Fy-Apple/nix.git ~/nix-darwin
 
-# clone the nix dotfile repository
-git clone https://github.com/fy-apple/nix-builder.git /etc/nix-darwin
-sudo chown $(id -nu):$(id -ng) /etc/nix-darwin
-cd /etc/nix-darwin
+# 4. create a symlink to /etc/nix-darwin
+sudo ln -s ~/nix-darwin /etc/nix-darwin
 
-
-# install th determined nix, choose no to avoid install nix for xxx
+# 5. install determined nix, choose no to avoid install nix for xxx
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
+# 6. Installing nix-darwin
 sudo nix run nix-darwin/master#darwin-rebuild -- switch
 
-# rebuild
+# 7. rebuild
 sudo darwin-rebuild switch --impure --show-trace --flake /etc/nix-darwin#HUAWEI-MateBook-Fold
